7e9dbf88e4478d2eb78e0e977f16c43919b43ddc
Kimseonghee <kimsunghee625@gmail.com>
2022-10-26 22:19:39 +0900

    justgram node CRUD-READ fix userPostDao sql finished

--- justgram_node/models/userPostDao.js
+++ justgram_node/models/userPostDao.js
@@ -1,28 +1,22 @@
-const { DataSource } = require("typeorm");
-
-const myDataSource = new DataSource({
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-});
-
-myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
-});
+const myDataSource = require("./index");
 
 const userPost = async (id) => {
-  const listInfo = await myDataSource.query(`SELECT
-  users.id as userId,
-  users.profile_image,
-  posting_images.posting_id,
-  postings.id as postingId,
-  postings.contents as postingContent,
-  posting_images.image_url as postingImageUrl
-  FROM users, postings, posting_images
-  WHERE users.id = 1 && postings.user_id = users.id && postings.id = posting_images.posting_id`);
+  const listInfo = await myDataSource.query(`
+  SELECT
+    users.id as userId,
+    users.profile_image as userProfileImage,
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+          "postingId", postings.id,
+          "postingImageUrl", posting_images.image_url,
+          "postingContent",postings.contents
+        )
+      )as postings
+  FROM users
+  JOIN postings ON postings.user_id = users.id
+  JOIN posting_images ON postings.id = posting_images.posting_id
+  WHERE users.id = ${id}
+  GROUP BY users.id`);
 
   return listInfo;
 };
